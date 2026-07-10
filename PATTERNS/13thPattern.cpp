f55bb97256800390fb4283dfdccbdd56586f9539
Divyanshu <divyanshusharma20062515@gmail.com>
2026-07-10 11:46:09 +0530

    added new Pattern

---
+++ PATTERNS/13thPattern.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void triangle(int n){
+    int num = 1;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<=i; j++){
+            cout << num << " ";
+            num +=1;
+        }
+        cout << endl;
+    }
+
+}
+
+int main(){
+    int n;
+    cout << "Enter Number of Rows : ";
+    cin >> n;
+    cout << endl;
+    triangle(n);
+
+    return 0;
+}

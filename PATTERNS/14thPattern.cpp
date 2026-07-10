026298b86419e99baaac08e1428c0e0d4e3a6f2d
Divyanshu <divyanshusharma20062515@gmail.com>
2026-07-10 11:53:12 +0530

    added character Pattern

---
+++ PATTERNS/14thPattern.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void triangle(int n){
+    for(int i=0; i<n; i++){
+        char letter = 'A';
+        for(int j=0; j<=i; j++){
+            cout << letter << " ";
+            letter++;
+        }
+        cout << endl;
+    }
+}
+
+int main(){
+    int n;
+    cout << "Enter A Number : "; cin >> n; cout << endl;
+    triangle(n);
+    return 0;
+}

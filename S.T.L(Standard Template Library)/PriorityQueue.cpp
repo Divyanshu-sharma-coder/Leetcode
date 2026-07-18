86cb5c63a215ed1f8dfc8da0dfecca0c61c8b48d
Divyanshu <divyanshusharma20062515@gmail.com>
2026-07-18 21:29:55 +0530

    added priorityQueue

--- S.T.L(Standard Template Library)/PriorityQueue.cpp
+++ S.T.L(Standard Template Library)/PriorityQueue.cpp
@@ -4,3 +4,52 @@
 // Job/Task Scheduling in Operating Systems: Computers manage multiple tasks simultaneously. Critical background processes or user commands jump ahead of idle background tasks.
 
 // VIP Customer Support: Companies like Zendesk often route premium or enterprise-tier tickets to the front of the queue, ensuring top-paying customers get serviced faster than standard account
+
+
+#include <iostream>
+#include <queue>
+
+// max priority Queue
+
+using namespace std;
+
+void Pqueue(){
+    priority_queue<int> max_pq;
+
+    max_pq.push(10);
+    max_pq.push(20);
+    max_pq.push(90);
+    max_pq.push(120);
+    max_pq.push(3);
+
+    cout << max_pq.top() << endl;
+    // output -- 120
+    cout << endl;
+    while(!max_pq.empty()){
+        cout << max_pq.top() << endl;
+        max_pq.pop();
+    }
+
+}
+void MinPqueue(){
+    cout << endl;
+    priority_queue<int, vector<int>, greater<int>> min_pq;
+    min_pq.push(12);
+    min_pq.push(293);
+    min_pq.push(233);
+        min_pq.push(2);
+            min_pq.push(-1);
+
+            while(!min_pq.empty()){
+        cout << min_pq.top() << endl;
+        min_pq.pop();
+    }
+
+
+
+}
+int main(){
+    Pqueue();
+    MinPqueue();
+    return 0;
+}

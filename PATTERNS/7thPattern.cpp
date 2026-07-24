bed3b6a6badacd63d296eec047d7c82734df1e7e
Divyanshu <divyanshusharma20062515@gmail.com>
2026-07-24 09:50:36 +0530

    added new patterns

--- PATTERNS/7thPattern.cpp
+++ PATTERNS/7thPattern.cpp
@@ -2,20 +2,16 @@
 using namespace std;
 
 void triangle(int n){
-    // Outer loop controls the current row (from 0 to n-1)
-    for(int row = 0; row < n; row++){
+    for(int row = 1; row <= n; row++){
         
-        // 1. Print leading spaces
-        for(int i = 0; i < n - row - 1; i++){
+        for(int i = 1; i <= n - row; i++){
             cout << " ";
         }
         
-        // 2. Print stars (formula for odd numbers: 2 * row + 1)
-        for(int j = 0; j < (2 * row) + 1; j++){
+        for(int j = 1; j <= (2 * row) - 1; j++){
             cout << "*";
         }
-        
-        // 3. Move to the next line (trailing spaces are unnecessary)
+
         cout << endl;
     }
 }

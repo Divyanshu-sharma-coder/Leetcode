afcc38aac831c166e3ba9d238102f4248808e294
Divyanshu <divyanshusharma20062515@gmail.com>
2026-07-26 12:03:20 +0530

    Added Reverse String and Valid Palindrome

--- Recursion/Programm3.cpp
+++ Recursion/Programm3.cpp
@@ -1,6 +1,8 @@
 // Reverse An Array
 #include <iostream>
 #include <algorithm>
+#include <string>
+#include <cctype>
 using namespace std;
 
 // void reverse(int arr[], int n){
@@ -25,6 +27,7 @@ using namespace std;
 //     return 0;
 // }
 
+// 1. Reverse an array using Recursion ----
 
 void Rev(int arr[], int l, int n, int r){
     // if (l>=r) return;
@@ -36,6 +39,27 @@ void Rev(int arr[], int l, int n, int r){
     Rev(arr, l+1, n, r);
 }
 
+
+// 2. Check weather a String is Palindrome or not---
+bool Palin(const string& s, int i, int d){
+    if(i>=d){
+        return true;
+    }
+
+        if (!isalnum(s[i])) {
+            return Palin(s, i + 1, d);
+        }
+
+        if (!isalnum(s[d])) {
+            return Palin(s, i, d - 1);
+        }
+    if(tolower(s[i])!=tolower(s[d])){
+        return false;
+    }
+
+    return Palin(s, i+1, d-1);
+}
+
 int main(){
     int arr[] = {1, 2, 3, 4, 5};
     int n = sizeof (arr)/ sizeof (arr[0]);
@@ -44,6 +68,18 @@ int main(){
     Rev(arr,l, n, r);
     for(int i=0; i<n; i++){
         cout << arr[i] << endl;
+    }
+    cout << endl;
+    string s;
+    cout << "ENter a String here : ";
+    cin >> s;
+    cout << endl;
+    int d = s.length()-1;
+    int i = 0;
+    if (Palin(s, i, d)){
+        cout << "Palindrome" << endl;
+    }else{
+        cout <<"Not a Palindrome" << endl;
     }
     return 0;
 }

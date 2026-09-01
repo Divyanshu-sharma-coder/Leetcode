2a9a0e28eb0e5cd65d94c404fe6d3a7211c2f43e
Divyanshu <divyanshusharma20062515@gmail.com>
2026-09-01 21:21:08 +0530

    added Selection sort

---
+++ Sorting/SelectionSort.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+void SelectionSort(int arr[], int n){
+    for(int i=0; i<=n-2; i++){
+        int small = i;
+        for(int j=i; j<=n-1; j++){
+            if(arr[j]<arr[small]){
+                small = j;
+            }
+        }
+        swap(arr[i], arr[small]);
+    }
+
+}
+
+int main(){
+    int arr[] = {12, 34, 73, 7273, 726, 8273, 82738263, 84, 272, 1829};
+    int n = sizeof (arr)/ sizeof (int);
+    SelectionSort(arr, n);
+    cout << "Sorted Array : " << endl;
+
+    for(int i=0; i<n; i++){
+        cout << arr[i] << " ";
+
+    }
+    cout << endl;
+    return 0;
+}
